b91ae948bafd6d98382c5943581b7dabd7adf1e2
dawidkopczyk <32499114+dawidkopczyk@users.noreply.github.com>
2018-02-04 12:34:15 +0100

    Add files via upload

---
+++ ae_mnist.py
@@ -0,0 +1,69 @@
+import matplotlib.pyplot as plt
+
+from keras.models import Model
+from keras.layers import (Input, Dense)
+
+# Load data
+from keras.datasets import mnist
+
+# input image dimensions
+img_rows, img_cols = 28, 28                          
+input_shape = (img_rows * img_cols, )
+
+# the data, shuffled and split between train and test sets
+(x_train, _), (x_test, _) = mnist.load_data()
+
+x_train = x_train.reshape(x_train.shape[0], img_rows * img_cols)
+x_test = x_test.reshape(x_test.shape[0], img_rows * img_cols)
+
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+
+print(x_train.shape[0], ' train samples')
+print(x_test.shape[0], ' test samples')
+    
+def AE(features_shape):
+
+    # Input
+    x = Input(name='inputs', shape=features_shape, dtype='float32')
+    o = x
+    
+    # Encoder
+    enc = Dense(32, activation='relu', name='encoder')(o)
+    
+    # Decoder
+    dec = Dense(features_shape[0], activation='sigmoid', name='decoder')(enc)
+    
+    # Print network summary
+    Model(inputs=x, outputs=dec).summary()
+    
+    return Model(inputs=x, outputs=dec)
+
+batch_size = 128
+epochs = 40
+
+autoenc = AE(input_shape)
+autoenc.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+autoenc.fit(x_train, x_train, epochs=epochs, batch_size=batch_size,
+            shuffle=True, validation_data=(x_test, x_test))
+
+decoded_imgs = autoenc.predict(x_test)
+
+n = 10
+plt.figure(figsize=(20, 4))
+for i in range(1,n+1):
+    # display original
+    ax = plt.subplot(2, n, i)
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    ax = plt.subplot(2, n, i + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
